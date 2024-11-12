88fe237694d36c5e429ce5fe9f1f9279429d8ce6
Eric Wijaya Lay <ericwijayalay@gmail.com>
2024-11-12 17:43:54 +0700

    setup migration and model

---
+++ app/Models/Diagnostic.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Diagnostic extends Model
+{
+    protected $fillable = [
+        'disease_name', 
+        'indication', 
+        'cause', 
+        'solution'];
+
+    public function diagnosticHistories()
+    {
+        return $this->hasMany(DiagnosticHistory::class);
+    }
+}
