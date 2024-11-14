0e2f980bbb8b8df4ab1dc7130a0ae783b939490e
Eric Wijaya Lay <ericwijayalay@gmail.com>
2024-11-14 22:18:16 +0700

    add setup for controller, action, and DTO

---
+++ app/Actions/Plant/StoreUserPlantAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Actions\Plant;
+
+use Holiq\ActionData\Foundation\Action;
+
+readonly class StoreUserPlantAction extends Action
+{
+    public function execute()
+    {
+        //
+    }
+}
---
+++ app/DataTransferObjects/Plant/UserPlantData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\DataTransferObjects\Plant;
+
+use Holiq\ActionData\Foundation\DataTransferObject;
+
+readonly class UserPlantData extends DataTransferObject
+{
+    final public function __construct(
+        //
+    ) {}
+}
