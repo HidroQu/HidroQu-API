84d1967f54b15336b5922edf35738cb4c83244e1
Eric Wijaya Lay <ericwijayalay@gmail.com>
2024-11-15 20:58:26 +0700

    Setup requests and DTO

--- app/Actions/Plant/StoreUserPlantAction.php
+++ app/Actions/Plant/StoreUserPlantAction.php
@@ -8,6 +8,13 @@ readonly class StoreUserPlantAction extends Action
 {
     public function execute(UserPlantData $data)
     {
+    /** @var \App\Models\User $user */
+    $user = auth()->user();
+
+    if ($user->id !== $data->user_id) {
+        abort(403, 'Unauthorized action.');
+    }
+
         return UserPlant::create([
             'user_id' => $data->user_id,
             'plant_id' => $data->plant_id,
