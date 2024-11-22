0ebcaf1bb2a9a1708c7ef34a611074f6f5ccec5f
holiq <holiq.ibrahim376@gmail.com>
2024-11-22 12:39:19 +0700

    feat(seeder): diagnostic history

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\DiagnosticHistory;
 use App\Models\User;
 use App\Models\UserPlant;
 use Illuminate\Database\Seeder;
@@ -28,15 +29,45 @@ class DatabaseSeeder extends Seeder
         UserPlant::query()->insert([
             'user_id' => $user->id,
             'plant_id' => 1,
-            'planting_date' => now(),
+            'planting_date' => now()->subDays(5),
             'notes' => 'ganti pupuk 1x seminggu',
         ]);
 
         UserPlant::query()->insert([
             'user_id' => $user->id,
             'plant_id' => 3,
-            'planting_date' => now()->subDays(3),
+            'planting_date' => now()->subDays(10),
             'notes' => 'ganti pupuk 1x seminggu',
+        ]);
+
+        DiagnosticHistory::query()->insert([
+            'user_plant_id' => 1,
+            'diagnostic_id' => 1,
+            'diagnosis_date' => now()->subDays(4),
+        ]);
+
+        DiagnosticHistory::query()->insert([
+            'user_plant_id' => 1,
+            'diagnostic_id' => 2,
+            'diagnosis_date' => now()->subDays(3),
+        ]);
+
+        DiagnosticHistory::query()->insert([
+            'user_plant_id' => 1,
+            'diagnostic_id' => 1,
+            'diagnosis_date' => now()->subDays(),
+        ]);
+
+        DiagnosticHistory::query()->insert([
+            'user_plant_id' => 2,
+            'diagnostic_id' => 2,
+            'diagnosis_date' => now()->subDays(5),
+        ]);
+
+        DiagnosticHistory::query()->insert([
+            'user_plant_id' => 2,
+            'diagnostic_id' => 1,
+            'diagnosis_date' => now()->subDays(2),
         ]);
     }
 }
