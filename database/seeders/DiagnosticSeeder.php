e1d119b8dc6cd37881801a0d27c39543d1528fff
holiq <holiq.ibrahim376@gmail.com>
2024-11-23 16:39:21 +0700

    feat(diagnostic): imrove seeds

--- database/seeders/DiagnosticSeeder.php
+++ database/seeders/DiagnosticSeeder.php
@@ -12,20 +12,56 @@ class DiagnosticSeeder extends Seeder
      */
     public function run(): void
     {
-        Diagnostic::create([
-            'disease_name' => 'Blight pada Timun',
-            'image_disease' => '',
-            'indication' => 'Daun berubah kuning dan bintik-bintik coklat.',
-            'cause' => 'Jamur yang berkembang di kelembapan tinggi.',
-            'solution' => 'Potong daun yang terinfeksi dan kurangi penyiraman.',
+        Diagnostic::query()->insert([
+            'disease_name' => 'Tanaman Sehat',
+            'disease_label' => 'Healthy',
+            'image_disease' => json_encode([
+                'https://storage.googleapis.com/hidroqu/diagnostics/sehat-1.png',
+                'https://storage.googleapis.com/hidroqu/diagnostics/sehat-2.png',
+                'https://storage.googleapis.com/hidroqu/diagnostics/sehat-3.png',
+            ]),
+            'indication' => 'Daun hijau merata, pertumbuhan tanaman optimal, dan tidak ada tanda-tanda kerusakan atau penyakit.',
+            'cause' => 'Kondisi lingkungan, nutrisi, dan perawatan yang memadai.',
+            'solution' => 'Pertahankan pola perawatan saat ini, pastikan penyiraman dan pemupukan dilakukan secara rutin.',
         ]);
 
-        Diagnostic::create([
-            'disease_name' => 'Busuk Akar pada Kangkung',
-            'image_disease' => '',
-            'indication' => 'Tanaman layu dan akar menjadi hitam dan lembek.',
-            'cause' => 'Disebabkan oleh kelembapan tanah yang berlebihan.',
-            'solution' => 'Kurangi penyiraman dan pastikan drainase yang baik.',
+        Diagnostic::query()->insert([
+            'disease_name' => 'Kekurangan Kalium',
+            'disease_label' => 'Kalium_Deficiency',
+            'image_disease' => json_encode([
+                'https://storage.googleapis.com/hidroqu/diagnostics/kalium-1.png',
+                'https://storage.googleapis.com/hidroqu/diagnostics/kalium-2.png',
+                'https://storage.googleapis.com/hidroqu/diagnostics/kalium-3.png',
+            ]),
+            'indication' => 'Daun menguning di tepinya, pertumbuhan buah kurang optimal, dan batang terlihat lemah.',
+            'cause' => 'Kurangnya kadar kalium dalam tanah atau kurangnya pemupukan kalium.',
+            'solution' => 'Gunakan pupuk kaya kalium seperti KCl atau pupuk kompos yang mengandung kalium.',
+        ]);
+
+        Diagnostic::query()->insert([
+            'disease_name' => 'Kekurangan Nitrogen',
+            'disease_label' => 'Nitrogen_Deficiency',
+            'image_disease' => json_encode([
+                'https://storage.googleapis.com/hidroqu/diagnostics/nitrogen-1.png',
+                'https://storage.googleapis.com/hidroqu/diagnostics/nitrogen-2.png',
+                'https://storage.googleapis.com/hidroqu/diagnostics/nitrogen-3.png',
+            ]),
+            'indication' => 'Daun tua menguning, pertumbuhan tanaman terhambat, dan batang terlihat kerdil.',
+            'cause' => 'Kekurangan nitrogen dalam tanah akibat pencucian atau pemupukan yang tidak cukup.',
+            'solution' => 'Tambahkan pupuk nitrogen seperti urea atau pupuk hijau secara berkala.',
+        ]);
+
+        Diagnostic::query()->insert([
+            'disease_name' => 'Kekurangan Fosfor',
+            'disease_label' => 'Phosphorus_Deficiency',
+            'image_disease' => json_encode([
+                'https://storage.googleapis.com/hidroqu/diagnostics/fosfor-1.png',
+                'https://storage.googleapis.com/hidroqu/diagnostics/fosfor-2.png',
+                'https://storage.googleapis.com/hidroqu/diagnostics/fosfor-3.png',
+            ]),
+            'indication' => 'Daun menguning keunguan, sistem perakaran lemah, dan pertumbuhan tanaman lambat.',
+            'cause' => 'Rendahnya kadar fosfor dalam tanah atau tanah yang terlalu asam.',
+            'solution' => 'Gunakan pupuk fosfat seperti TSP (Triple Superphosphate) atau tambahkan kapur untuk menetralkan keasaman tanah.',
         ]);
     }
 }
